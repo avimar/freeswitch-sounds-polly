dbab0488f1b57eee3700b1e47d79d33bbecb51bd
Avi Marcus <avi@bestfone.com>
2020-05-07 16:26:45 +0300

    fix typo

--- phrases-class.js
+++ phrases-class.js
@@ -1,30 +1,30 @@
 module.exports = [
-{fileame: "enter-class-number", phrase: "Please enter your class number, followed by the pound key."}
-,{fileame: "enter-class-number-7", phrase: "Please enter your 7 digit class number."}
-,{fileame: "bad-class-number", phrase: "Invalid class number, please try again."}
+{filename: "enter-class-number", phrase: "Please enter your class number, followed by the pound key."}
+,{filename: "enter-class-number-7", phrase: "Please enter your 7 digit class number."}
+,{filename: "bad-class-number", phrase: "Invalid class number, please try again."}
 
 ,{phrase: 'Please hold on the line for your class to start.', filename:'hold-wait-teacher'}
 ,{phrase: 'You are currently the only person in this conference. Please wait for others to join.',filename:'hold-wait-for-others'}
 
-,{fileame: "hand-raised", phrase:"Your hand is now raised."}
-,{fileame: "hand-lowered", phrase: "Your hand is now lowered."}
-,{fileame: "you-are-number", phrase: "You are num-ber"} //weird! it drops the B for some reason in "number"
-,{fileame: "listeners", phrase: "listeners"}
-,{fileame: "listener", phrase: "listener"}
+,{filename: "hand-raised", phrase:"Your hand is now raised."}
+,{filename: "hand-lowered", phrase: "Your hand is now lowered."}
+,{filename: "you-are-number", phrase: "You are num-ber"} //weird! it drops the B for some reason in "number"
+,{filename: "listeners", phrase: "listeners"}
+,{filename: "listener", phrase: "listener"}
 
-,{fileame: "question-asked", phrase: "Question!"}
-//{fileame: /"question-askedSoft", phrase: '<speak><amazon:effect phonation="soft">Question!</amazon:effect></speak>'} // NON-neural!
+,{filename: "question-asked", phrase: "Question!"}
+//{filename: /"question-askedSoft", phrase: '<speak><amazon:effect phonation="soft">Question!</amazon:effect></speak>'} // NON-neural!
 
-,{fileame: "no-questions-removed", phrase: "No questions removed."}
-,{fileame: "questions-removed", phrase: "questions removed"}
-,{fileame: "1-question-removed", phrase: "one question removed"}
+,{filename: "no-questions-removed", phrase: "No questions removed."}
+,{filename: "questions-removed", phrase: "questions removed"}
+,{filename: "1-question-removed", phrase: "one question removed"}
 
-,{fileame: "no-questions", phrase: "No questions."}
-,{fileame: "questions", phrase: "questions"}
-,{fileame: "1-question", phrase: "one question"}
+,{filename: "no-questions", phrase: "No questions."}
+,{filename: "questions", phrase: "questions"}
+,{filename: "1-question", phrase: "one question"}
 
 
-,{fileame: "all-students-muted", phrase: "All students are now muted."}
-,{fileame: "line-open", phrase: "Line is now open."}
-,{fileame: "line-closed", phrase: "Line is now closed."}
+,{filename: "all-students-muted", phrase: "All students are now muted."}
+,{filename: "line-open", phrase: "Line is now open."}
+,{filename: "line-closed", phrase: "Line is now closed."}
 ];
